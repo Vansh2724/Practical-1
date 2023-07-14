5e7e71f5b0219e647ab003bfd6c4f3dee498eeb0
MAD_Practical-1 <vanshpatel2724@gmail.com>
2023-07-14 12:37:07 +0530

    1st time

---
+++ src/main/kotlin/Practical_1.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    println("Hello World!")
+    val int_num = 5 //Int
+    val float_num = 505 //float
+    val double_num = 6.68 //double
+    val word_char = 'V' //char
+    val my_bool = true //boolean
+    val my_text = "Vansh" //String
+    val my_long: Long = 4556475641 //long
+    val my_short : Short = -89 //Short
+    val my_byte : Byte = 125 //Byte
+}
